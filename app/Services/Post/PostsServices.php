fca93bba3d4578b1aa094bf691d78c1cb20a051c
ramosramosramos <ramosramosramosramos12@gmail.com>
2024-09-15 23:49:41 +0800

    reaction  done

--- app/Services/Post/PostsServices.php
+++ app/Services/Post/PostsServices.php
@@ -16,27 +16,39 @@ class PostsServices
         $postsQuery = $onlyTrashed ? Post::onlyTrashed() : Post::withoutTrashed()->withReactionCounts();
 
         $posts = $postsQuery
-        ->where('user_id', request()->user()->id)
-        ->where('isPinned', $isPinned)
-        ->latest()
-        ->get()
-        ->map(function ($post) use ($time) {
-            return [
-                'id' => $post->id,
-                'user_id' => $post->user_id,
-                'caption' => $post->caption,
-                'content' => $post->content,
-                'time' => Carbon::parse($post->$time)->diffForHumans(),
-                'reactions' => [
-                    'heart' => $post->heart_count,
-                    'happy' => $post->happy_count,
-                    'dislike' => $post->dislike_count,
-                    'mad' => $post->mad_count,
-                    'sad' => $post->sad_count,
-                    'total' => $post->reactions_count
-                ]
-            ];
-        });
+            ->where('user_id', request()->user()->id)
+            ->where('isPinned', $isPinned)
+            ->latest()
+            ->get()
+            ->map(function ($post) use ($time) {
+
+                if ($time=='created_at') {
+                    return [
+                        'id' => $post->id,
+                        'user_id' => $post->user_id,
+                        'caption' => $post->caption,
+                        'content' => $post->content,
+                        'time' => Carbon::parse($post->$time)->diffForHumans(),
+                        'reactions' => [
+                            'heart' => $post->heart_count,
+                            'happy' => $post->happy_count,
+                            'dislike' => $post->dislike_count,
+                            'mad' => $post->mad_count,
+                            'sad' => $post->sad_count,
+                            'total' => $post->reactions_count
+                        ]
+                    ];
+
+                }
+
+                return [
+                    'id' => $post->id,
+                    'user_id' => $post->user_id,
+                    'caption' => $post->caption,
+                    'content' => $post->content,
+                    'time' => Carbon::parse($post->$time)->diffForHumans(),
+                ];
+            });
 
         return $posts;
     }
